d695a700080b49abaabe0e911445bea5df4b30f5
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-23 02:48:09 +0700

    update gitignore

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -5,7 +5,7 @@ const connectMongoDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB.");
     } catch (error) {
-        console.log(error);
+        console.log("Error connecting to MongoDB: ",error);
     }
 };
  
